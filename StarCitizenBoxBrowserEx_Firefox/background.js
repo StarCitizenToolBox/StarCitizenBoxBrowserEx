d6192cd16576891882b366025e50edfd76d18ea7
xkeyC <3334969096@qq.com>
2023-12-09 13:04:15 +0800

    更换服务器地址

--- StarCitizenBoxBrowserEx_Firefox/background.js
+++ StarCitizenBoxBrowserEx_Firefox/background.js
@@ -108,7 +108,7 @@ function getLocalizationResource(localizationResource, key) {
 }
 
 async function _getJsonData(fileName, { cacheKey = "", version = null } = {}) {
-    url = "https://ch.citizenwiki.cn/json-files/locales/" + fileName;
+    url = "https://git.sctoolbox.sccsgo.com/SCToolBox/ScWeb_Chinese_Translate/raw/branch/main/json/locales/" + fileName;
     if (cacheKey && cacheKey !== "") {
         const localVersion = await getLocalData(`${cacheKey}_version`);
         const data = await getLocalData(cacheKey);
